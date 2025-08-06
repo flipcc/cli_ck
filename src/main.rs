15f259c5e8c098723d27c554eab9141c2955d28c
ext-pau-mue <85668376+ext-pau-mue@users.noreply.github.com>
2025-08-06 15:36:06 +0200

    Add cpal crate successfully

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use cpal::traits::{DeviceTrait, HostTrait};
+
 fn main() {
-    println!("Hello, world!");
+    let host = cpal::default_host();
+    let device = host
+        .default_output_device()
+        .expect("Failed to find output device");
+
+    println!("Output device: {}", device.name().unwrap());
 }
